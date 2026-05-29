cfbfa0728bdf6a5dc3628a2129da69c5d6db5bfb
sugi-road <sugi.road@gmail.com>
2026-05-29 22:35:48 +0900

    Update quiz2026.js

--- data/quiz2026.js
+++ data/quiz2026.js
@@ -2,32 +2,32 @@ const quiz2026 = {
 
   players:[
 
- {no:1,name:"西村 清花(にしむら きよか)"  ,from:"兵庫県"		,pos:"GK"	,company:"イーグリッド"		,join:2024	,call:"きよ"},
- {no:2,name:"渡部 那月(わたなべ なつき)"  ,from:"出雲市"	  ,pos:"DF"	,company:"建装"	      	,join:2026	,call:"なつき"},
- {no:3,name:"オモロジェバ 英里香(えりか)"  ,from:"東京都"	,pos:"DF"	,company:"サン電子工業"		,join:2026	,call:"おもろ"},
- {no:4,name:"伊藤 明里(いとう あかり)"    ,from:"広島県"		,pos:"MF"	,company:"ナカバヤシ ファクトリー"	  	,join:2020	,call:"あかり"},
- {no:5,name:"安藤 真桜(あんどう まお)"    ,from:"鳥取県"		,pos:"DF"	,company:"ナカバヤシ ファクトリー"	  	,join:2023	,call:"まお"},
- {no:6,name:"伊勢 さつき(いせ さつき)"    ,from:"大阪府"		,pos:"MF"	,company:"サン電子工業"		,join:2025	,call:"さつき"},
- {no:7,name:"丸尾 梨菜(まるお りな)"      ,from:"兵庫県"	  	,pos:"FW"	,company:"出雲村田製作所"	,join:2024	,call:"りな"},
- {no:8,name:"友近 真那(ともちか まな)"    ,from:"愛媛県"		,pos:"DF"	,company:"出雲村田製作所"	,join:2020	,call:"まな"},
- {no:9,name:"河村 祐実(かわむら ゆうみ)"  ,from:"兵庫県"		,pos:"FW"	,company:"姿勢堂"	      	,join:2024	,call:"ゆうみ"},
- {no:10,name:"原 美埜里(はら みのり)"    ,from:"出雲市"	  	,pos:"MF"	,company:"島根銀行"	    	,join:2024	,call:"みのり"},
- {no:11,name:"宮武 里奈(みやたけ りな)"  ,from:"福岡県"		,pos:"DF"	,company:"出雲グリーン"		,join:2024	,call:"たけ"},
- {no:13,name:"打越 怜(うちこし れん)"    ,from:"佐賀県"	  	,pos:"MF"	,company:"アルファー食品"	,join:2025	,call:"れん･れん･れん"},
- {no:14,name:"恒石 亜弓(つねいし あゆみ)" ,from:"大阪府"	,pos:"MF"	,company:"山陰ヤクルト販売"	    	,join:2024	,call:"あゆみ"},
- {no:15,name:"佐藤 萌里(さとう もえり)"  ,from:"神奈川県"	,pos:"MF"	,company:"山陰ヤクルト販売"	    	,join:2025	,call:"もえり"},
- {no:16,name:"相浦 幸桜(あいうら しあら)"  ,from:"福岡県"	,pos:"MF"	,company:"CSいずも"	    	,join:2025	,call:"しあら"},
- {no:17,name:"島内 日菜子(しまうち ひなこ)",from:"佐賀県"	,pos:"DF"	,company:"CSいずも"	  	,join:2025	,call:"ひなこ"},
- {no:18,name:"福井 真歩(ふくい まほ)"    ,from:"千葉県"	  	,pos:"MF"	,company:"TSK農縁"     	,join:2026	,call:"まほ"},
- {no:19,name:"平良 文果(たいら ふみか)"  ,from:"沖縄県"		,pos:"DF"	,company:"LCC"	      	,join:2026	,call:"ふみか"},
- {no:20,name:"都筑 かんな(つづき かんな)",from:"愛知県"	,pos:"FW"	,company:"出雲村田製作所"	,join:2026	,call:"かんな"},
- {no:21,name:"尾添 有紗(おぞえ ありさ)"  ,from:"出雲市"		,pos:"GK"	,company:"島根銀行"	    	,join:2024	,call:"ありさ"},
- {no:22,name:"徳本 華暖(とくもと かのん)",from:"京都府"	,pos:"DF"	,company:"島建コンサルタント"	,join:2026	,call:"かのん"},
- {no:23,name:"野村 彩衣(のむら さえ)"    ,from:"愛知県"	  	,pos:"DF"	,company:"福間商事"	    	,join:2024	,call:"さえ"},
- {no:30,name:"松本 悠愛(まつもと ゆうな)",from:"鳥取県"	,pos:"FW"	,company:"境港二中"	    	,join:2026	,call:"ゆうな"},
- {no:31,name:"金織 玲七(かなおり れな)"  ,from:"松江市"		,pos:"GK"	,company:"大社高"	      	,join:2026	,call:"れな"},
- {no:32,name:"竹下 七海(たけした ななみ)"  ,from:"松江市"	,pos:"DF"	,company:"松江西高"	    	,join:2026	,call:"ななみ"},
- {no:33,name:"高木 菜望(たかき なみ)"    ,from:"松江市"	  	,pos:"DF"	,company:"開星中"	      	,join:2026	,call:"なみ"}
+ {no:1,name:"西村 清花(にしむら きよか)"  ,from:"兵庫県"	,pos:"GK"	,company:"イーグリッド"		,join:2024	,call:"きよ"	,face:"images/face1.png"},
+ {no:2,name:"渡部 那月(わたなべ なつき)"  ,from:"出雲市"	,pos:"DF"	,company:"建装"	      		,join:2026	,call:"なつき"	,face:"images/face2.png"},
+ {no:3,name:"オモロジェバ 英里香(えりか)"  ,from:"東京都"	,pos:"DF"	,company:"サン電子工業"		,join:2026	,call:"おもろ"	,face:"images/face3.png"},
+ {no:4,name:"伊藤 明里(いとう あかり)"    ,from:"広島県"	,pos:"MF"	,company:"ナカバヤシ ファクトリー" ,join:2020	,call:"あかり"	,face:"images/face4.png"},
+ {no:5,name:"安藤 真桜(あんどう まお)"    ,from:"鳥取県"	,pos:"DF"	,company:"ナカバヤシ ファクトリー" ,join:2023	,call:"まお"	,face:"images/face5.png"},
+ {no:6,name:"伊勢 さつき(いせ さつき)"    ,from:"大阪府"	,pos:"MF"	,company:"サン電子工業"		,join:2025	,call:"さつき"	,face:"images/face6.png"},
+ {no:7,name:"丸尾 梨菜(まるお りな)"      ,from:"兵庫県"	,pos:"FW"	,company:"出雲村田製作所"	,join:2024	,call:"りな"	,face:"images/face7.png"},
+ {no:8,name:"友近 真那(ともちか まな)"    ,from:"愛媛県"	,pos:"DF"	,company:"出雲村田製作所"	,join:2020	,call:"まな"	,face:"images/face8.png"},
+ {no:9,name:"河村 祐実(かわむら ゆうみ)"  ,from:"兵庫県"	,pos:"FW"	,company:"姿勢堂"	      	,join:2024	,call:"ゆうみ"	,face:"images/face9.png"},
+ {no:10,name:"原 美埜里(はら みのり)"    ,from:"出雲市"	,pos:"MF"	,company:"島根銀行"	    	,join:2024	,call:"みのり"	,face:"images/face10.png"},
+ {no:11,name:"宮武 里奈(みやたけ りな)"  ,from:"福岡県"	,pos:"DF"	,company:"出雲グリーン"		,join:2024	,call:"たけ"	,face:"images/face11.png"},
+ {no:13,name:"打越 怜(うちこし れん)"    ,from:"佐賀県"	,pos:"MF"	,company:"アルファー食品"	,join:2025	,call:"れん･れん･れん",face:"images/face13.png"},
+ {no:14,name:"恒石 亜弓(つねいし あゆみ)" ,from:"大阪府"	,pos:"MF"	,company:"山陰ヤクルト販売"	,join:2024	,call:"あゆみ"	,face:"images/face14.png"},
+ {no:15,name:"佐藤 萌里(さとう もえり)"  ,from:"神奈川県"	,pos:"MF"	,company:"山陰ヤクルト販売"	,join:2025	,call:"もえり"	,face:"images/face15.png"},
+ {no:16,name:"相浦 幸桜(あいうら しあら)"  ,from:"福岡県"	,pos:"MF"	,company:"CSいずも"	    	,join:2025	,call:"しあら"	,face:"images/face16.png"},
+ {no:17,name:"島内 日菜子(しまうち ひなこ)",from:"佐賀県"	,pos:"DF"	,company:"CSいずも"	  	,join:2025	,call:"ひなこ"	,face:"images/face17.png"},
+ {no:18,name:"福井 真歩(ふくい まほ)"    ,from:"千葉県"	,pos:"MF"	,company:"TSK農縁"     		,join:2026	,call:"まほ"	,face:"images/face18.png"},
+ {no:19,name:"平良 文果(たいら ふみか)"  ,from:"沖縄県"	,pos:"DF"	,company:"LCC"	      		,join:2026	,call:"ふみか"	,face:"images/face19.png"},
+ {no:20,name:"都筑 かんな(つづき かんな)",from:"愛知県"	,pos:"FW"	,company:"出雲村田製作所"	,join:2026	,call:"かんな"	,face:"images/face20.png"},
+ {no:21,name:"尾添 有紗(おぞえ ありさ)"  ,from:"出雲市"	,pos:"GK"	,company:"島根銀行"	    	,join:2024	,call:"ありさ"	,face:"images/face21.png"},
+ {no:22,name:"徳本 華暖(とくもと かのん)",from:"京都府"	,pos:"DF"	,company:"島建コンサルタント"	,join:2026	,call:"かのん"	,face:"images/face22.png"},
+ {no:23,name:"野村 彩衣(のむら さえ)"    ,from:"愛知県"	,pos:"DF"	,company:"福間商事"	    	,join:2024	,call:"さえ"	,face:"images/face23.png"},
+ {no:30,name:"松本 悠愛(まつもと ゆうな)",from:"鳥取県"	,pos:"FW"	,company:"境港二中"	    	,join:2026	,call:"ゆうな"	,face:"images/face30.png"},
+ {no:31,name:"金織 玲七(かなおり れな)"  ,from:"松江市"	,pos:"GK"	,company:"大社高"	      	,join:2026	,call:"れな"	,face:"images/face31.png"},
+ {no:32,name:"竹下 七海(たけした ななみ)"  ,from:"松江市"	,pos:"DF"	,company:"松江西高"	    	,join:2026	,call:"ななみ"	,face:"images/face32.png"},
+ {no:33,name:"高木 菜望(たかき なみ)"    ,from:"松江市"	,pos:"DF"	,company:"開星中"	      	,join:2026	,call:"なみ"	,face:"images/face33.png"}
 
   ],
 
@@ -40,7 +40,8 @@ const quiz2026 = {
       questions:[
         "number",
         "position",
-        "call"
+        "call",
+	"face"
       ]
     },
 
